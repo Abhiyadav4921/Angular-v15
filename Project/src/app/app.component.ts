8845e7d08c32de122d82ba6bdfea23b8f7916355
simran.sachdeva <simran.sachdeva@bluekaktus.com>
2024-11-20 17:40:09 +0530

    updated

--- Project/src/app/app.component.ts
+++ Project/src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
+import { JsonPipe } from '@angular/common';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +10,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Project';
+//   posts: any[] = [];
+//   constructor(private http:HttpClient, private apiService: ApiService) {
+//     this.loadPosts();
+//   }
+//   loadPosts() {
+//     this.http.get('https://jsonplaceholder.typicode.com/posts').subscribe((responce) => {
+//       alert(JSON.stringify(responce));
+//   });
+//  }
 }
